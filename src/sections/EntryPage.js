2197aa3c4e9e7262910cfd3a569e40b469587ff2
Lavina <129137303+WHT-RBT@users.noreply.github.com>
2023-09-02 22:15:36 -0700

    updated code - still has errors

--- src/sections/EntryPage.js
+++ src/sections/EntryPage.js
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
 import keyImage from '../assets/key.png';
 import keyholeImage from '../assets/keyhole.png';
+import entryVideo from '../assets/entryvideo.mp4';
+import song1 from '../assets/song1.mp3';
 import Ripples from 'react-ripples';
 
 const EntryPage = ({ onEnter }) => {
@@ -8,35 +10,33 @@ const EntryPage = ({ onEnter }) => {
     const audioRef = useRef(null);
     const [started, setStarted] = useState(false);
     const [showRippleButton, setShowRippleButton] = useState(false);
-    const songs = ["song1.mp3", "song2.mp3", "song3.mp3", "song4.mp3", "song5.mp3", "song6.mp3"];
 
     const handleKeyholeClick = () => {
         setStarted(true);
-        if (videoRef.current && audioRef.current) {
-            videoRef.current.play().catch(error => console.log("Video play failed:", error));
-            audioRef.current.play().catch(error => console.log("Audio play failed:", error));
-        }
     };
 
     useEffect(() => {
-        if (started) {
-            const handleVideoEnd = () => {
-                setShowRippleButton(true);
-            };
-    
-            const currentVideoRef = videoRef.current; // Store current ref in a variable
-    
-            if (currentVideoRef) {
-                currentVideoRef.addEventListener('ended', handleVideoEnd);
-    
-                // Cleanup function
-                return () => {
-                    if (currentVideoRef) {
-                        currentVideoRef.removeEventListener('ended', handleVideoEnd);
-                    }                };
-            }
+        if (started && videoRef.current && audioRef.current) {
+            videoRef.current.play().catch(error => console.log("Video play failed:", error));
+            audioRef.current.play().catch(error => console.log("Audio play failed:", error));
         }
     }, [started]);
+
+    useEffect(() => {
+        const handleVideoEnd = () => {
+            setShowRippleButton(true);
+        };
+
+        const currentVideoRef = videoRef.current;
+
+        if (currentVideoRef) {
+            currentVideoRef.addEventListener('ended', handleVideoEnd);
+
+            return () => {
+                currentVideoRef.removeEventListener('ended', handleVideoEnd);
+            };
+        }
+    }, []);
 
     const handleRippleClick = () => {
         onEnter(); // Navigate to Home
@@ -45,19 +45,19 @@ const EntryPage = ({ onEnter }) => {
     return (
         <div className="entry-page" style={{ backgroundColor: 'black' }}>
             {!started ? (
-                <div className="keyhole-container" 
-                     onClick={handleKeyholeClick} 
-                     style={{ cursor: `url(${keyImage}), auto` }}>
+                <div className="keyhole-container"
+                    onClick={handleKeyholeClick}
+                    style={{ cursor: `url(${keyImage}), auto` }}>
                     <img src={keyholeImage} alt="" />
                     <span className="unlock-text">Unlock Me</span>
                 </div>
             ) : (
                 <>
-                    <video ref={videoRef} muted className="video-background">
-                        <source src="/assets/entryvideo.mp4" type="video/mp4" />
+                    <video ref={videoRef} muted className="video">
+                        <source src={entryVideo} type="video/mp4" />
                     </video>
                     <audio ref={audioRef} loop={false} className="hidden">
-                        <source src={`/assets/${songs[0]}`} type="audio/mp3" />
+                        <source src={song1} type="audio/mp3" />
                     </audio>
                     {showRippleButton && (
                         <Ripples color="#007bff">
