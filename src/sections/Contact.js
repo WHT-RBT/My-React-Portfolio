0ef53a6388e023eb9cade019a30c9d4609842ea8
Lavina <129137303+WHT-RBT@users.noreply.github.com>
2023-09-04 13:15:35 -0700

    more changes to contact section

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -41,7 +41,7 @@ const ContactForm = () => {
         return (
             <>
                 <div className="text-2xl">Thank you!</div>
-                <div className="text-md">We'll be in touch soon.</div>
+                <div className="text-md">We will be in touch soon!</div>
             </>
         );
     }
@@ -119,7 +119,7 @@ const ContactVideo = () => {
                     <source src={whtrbtVideo} type="video/mp4" />
                 </video>
             )}
-            <ContactForm /> 
+            <ContactForm />
         </div>
     );
 };
