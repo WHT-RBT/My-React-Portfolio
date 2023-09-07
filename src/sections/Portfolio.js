437a0610bcf56a1cddbccf4f5ee431349caa3b78
Lavina <129137303+WHT-RBT@users.noreply.github.com>
2023-09-06 22:28:56 -0700

    small changes

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -14,67 +14,67 @@ import MyPortfolio from '../assets/Portfolio.png';
 const Portfolio = () => {
   return (
     <div>
-   <div>
-   <header style={{ width: '100%', marginTop: '11%' }} className="header-center">
-  <div className="header-container">
-    <img
-      src={MyPortfolio}
-      alt="MyPortfolio"
-      className="header"
-      style={{ width: '100%', height: 'auto' }}
-    />
-  </div>
-</header>
+      <div>
+        <header style={{ width: '100%', marginTop: '11%' }} className="header-center">
+          <div className="header-container">
+            <img
+              src={MyPortfolio}
+              alt="MyPortfolio"
+              className="header"
+              style={{ width: '100%', height: 'auto' }}
+            />
+          </div>
+        </header>
 
-      <div className="portfolio-section">
+        <div className="portfolio-section">
 
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }} >
-          <a href="https://github.com/WHT-RBT/06-Side-APIs-Challenge-Weather-Dashboard.git">
-            <img src={WeatherDashboard} alt="WeatherDashboard" className="resized-image" />
-          </a>
-        </div>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }} >
+            <a href="https://github.com/WHT-RBT/06-Side-APIs-Challenge-Weather-Dashboard.git">
+              <img src={WeatherDashboard} alt="WeatherDashboard" className="resized-image" />
+            </a>
+          </div>
 
 
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
-          <a href="https://github.com/WHT-RBT/Dish-Hunt.git">
-            <img src={DishHunt} alt="DishHunt" className="resized-image" />
-          </a>
-        </div>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
+            <a href="https://github.com/WHT-RBT/Dish-Hunt.git">
+              <img src={DishHunt} alt="DishHunt" className="resized-image" />
+            </a>
+          </div>
 
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
-          <a href="https://github.com/WHT-RBT/TimeKillerProject">
-            <img src={TimeKiller} alt="TimeKiller" className="resized-image" />
-          </a>
-        </div>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
+            <a href="https://github.com/WHT-RBT/TimeKillerProject">
+              <img src={TimeKiller} alt="TimeKiller" className="resized-image" />
+            </a>
+          </div>
 
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
-          <a href="https://wht-rbt.github.io/React-BucketList/">
-            <img src={BucketList} alt="BucketList" className="resized-image" />
-          </a>
-        </div>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
+            <a href="https://wht-rbt.github.io/React-BucketList/">
+              <img src={BucketList} alt="BucketList" className="resized-image" />
+            </a>
+          </div>
 
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
-          <a href="https://www.barnesandnoble.com/w/bunnyville-the-alphabet-lm-castillo/1143847976?ean=2940160956442">
-            <img src={Bunnyville} alt="Bunnyville" className="resized-image" />
-          </a>
-        </div>
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
-          <a href="https://www.barnesandnoble.com/w/books/1143960930?ean=2940185934609">
-            <img src={TheJourney} alt="TheJourney" className="resized-image" />
-          </a>
-        </div>
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
-          <a href="https://madewithlovebylmc.etsy.com">
-            <img src={MadeWithLove} alt="MadeWithLove" className="resized-image" />
-          </a>
-        </div>
-        <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
-          <a href="https://dreamonaz.com">
-            <img src={DreamOn} alt="DreamOn" className="resized-image" />
-          </a>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
+            <a href="https://www.barnesandnoble.com/w/bunnyville-the-alphabet-lm-castillo/1143847976?ean=2940160956442">
+              <img src={Bunnyville} alt="Bunnyville" className="resized-image" />
+            </a>
+          </div>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
+            <a href="https://www.barnesandnoble.com/w/books/1143960930?ean=2940185934609">
+              <img src={TheJourney} alt="TheJourney" className="resized-image" />
+            </a>
+          </div>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
+            <a href="https://madewithlovebylmc.etsy.com">
+              <img src={MadeWithLove} alt="MadeWithLove" className="resized-image" />
+            </a>
+          </div>
+          <div className="portfolio-item" style={{ backgroundColor: 'white' }}>
+            <a href="https://dreamonaz.com">
+              <img src={DreamOn} alt="DreamOn" className="resized-image" />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
